0cb1093b673526f07e106e6049c567a42e8fd2f5
Tyler van der Hoeven <hi@tyvdh.com>
2024-03-18 16:54:56 -0400

    remove unused part of test

--- tests/index.ts
+++ tests/index.ts
@@ -4,59 +4,6 @@ const path = "./snapshot.json";
 const file = Bun.file(path);
 const snapshot = await file.json();
 
-// const contract_a = Address.fromScAddress(xdr.ScAddress.scAddressTypeContract(Buffer.from(new Array(32).fill(0)))).toString();
-
-// const contract_b_array = new Array(32).fill(1)
-// const contract_b = Address.fromScAddress(xdr.ScAddress.scAddressTypeContract(Buffer.from(contract_b_array))).toString();
-
-// const json: any = {
-//     contract_id: contract_b_array,
-//     fname: "add_with",
-//     ledger_sequence: 0,
-//     params: [
-//         {
-//             address: contract_a
-//         },
-//         {
-//             u32: 5
-//         },
-//         {
-//             u32: 15
-//         }
-//     ],
-//     keys: [],
-//     vals: []
-// }
-
-// const swaps = [
-//     ['CBKMUZNFQIAL775XBB2W2GP5CNHBM5YGH6C3XB7AY6SUVO2IBU3VYK2V', contract_a],
-//     ['CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFCT4', contract_b],
-//     // ['61f3ec45073b00a29bce98268932a9dcfa4f803efd2a425e0938d44047608b4d', '61f3ec45073b00a29bce98268932a9dcfa4f803efd2a425e0938d44047608b4d'], // contract_a (unsure why this is identical between testing and soroflare but contract_b isn't)
-//     ['e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', 'ba45dcc500ee79e4e6dad8017a74b25341ff527529677d5c24948cbfb7676e94'] // contract_b
-// ]
-
-// snapshot.ledger.ledger_entries.forEach((element: any, index: number) => {
-//     let [key, val] = element
-
-//     if (
-//         key.contract_code
-//         || JSON.stringify(key).includes('"durability":"temporary"')
-//     ) return
-
-//     swaps.forEach(([a, b]) => {
-//         key = JSON.parse(JSON.stringify(key).replace(new RegExp(a, 'g'), b))
-//         val = JSON.parse(JSON.stringify(val).replace(new RegExp(a, 'g'), b))
-//     })
-
-//     let [entry, live_until] = val
-
-//     json.keys.push(key)
-//     json.vals.push({
-//         entry,
-//         live_until
-//     })
-// });
-
 const json = {
     "adjustment_config": {
         "instructions": {
@@ -116,7 +63,7 @@ const json = {
     ],
     "fname": "add_with",
     "ledger_entries": snapshot.ledger.ledger_entries,
-    "ledger_sequence": 0,
+    "ledger_sequence": 5000,
     "network": "Test SDF Network ; September 2015",
     "network_config": null,
     "params": [
